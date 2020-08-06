f147a0a6d8709eb169da908f2c65f9fdb4fa35ff
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-08-06 17:28:07 -0300

    add adjustments on give-classes page

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-/* EXPRESS = Library of frameworks to run a properly server. */
-/* Nunjucks = Template Engine which allows us to manipulate a html easily * /
+// EXPRESS = Library of frameworks to run a properly server-side. //
+// Nunjucks = Template Engine which allows us to manipulate our html easily. //
 
-/* const var to shortly call express when it's needed */
+// Const var to shortly refer express when it's needed. //
 const express = require('express');
-/* const var to server expressions use */
+// Const var to server expressions use. //
 const server = express();
-/* nunjucks configuration */
+// Nunjucks configuration. //
+// Basically we are refering which dir has the html that will be futher manipulated. //
 const nunjucks = require('nunjucks');
 nunjucks.configure('src/views', {
     express: server,
     noCache: true
 })
 
-
+// Array of Proffys (teachers). //
 const proffys = [
     { 
         name: "Gabriel Werner", 
@@ -37,7 +38,7 @@ const proffys = [
         time_to: [1220]
     }
 ]
-
+// Array of available subjects. //
 const subjects = [
     "Artes",
     "Biologia",
@@ -50,7 +51,7 @@ const subjects = [
     "Portugês",
     "Química",
 ];
-
+// Array of Weekdays. //
 const weekdays = [
     "Domingo",
     "Segunda-feira",
@@ -61,39 +62,61 @@ const weekdays = [
     "Sábado"
 ];
 
-/* setting the route 'get', to return the second parameter */
-/* 'req' is basically the info you are sending to the endpoint */ 
-/* 'res' is the return of this endpoint */
 
+// ROUTES INFO:
+// 'req' is basically the info you are sending to the endpoint 
+// 'res' is the return of this endpoint
+
+// Main Page config //
 function pageLanding(req, res) {
     return res.render("index.html")
 }
+
+// Study Page config //
 function pageStudy(req, res) {
     const filters = req.query;
     return res.render("study.html", {proffys, filters, subjects, weekdays})
 }
-function pageGiveClasses(req, res) {
-    const data = req.query;
 
+// this function takes the index of the subject and return his name //
+function getSubject(subjectNumber) {
+    const position = +subjectNumber -1;
+    return subjects[position];
+}
+
+// Give-Classes Page Config //
+function pageGiveClasses(req, res) {
+
+    // Storing the data that we are sending through the 'req' parameter //
+    // basically this 'req' will have all form data from the Give-Classes //
+    // when the 'save' button is clicked. //
+    const data = req.query;
+    // Checking if the data returned is not null (empty) //
     const isNotEmpty = Object.keys(data).length > 0;
 
+    // If the data is complete, we are going to record this new Proffy user //
     if(isNotEmpty) {
+        // Consulting the name of the subject, using the function bellow. //
+        data.subject = getSubject(data.subject);
+        // Pushing the new data into the Proffys array. //
         proffys.push(data);
-        return res.redirect("/study")
+        // Redirect to the Study page. //
+        return res.redirect("/study");
     }
     
     return res.render("give-classes.html", {subjects, weekdays})
 }
 
-/* every '.use()' function its a server config settings */
-/* config the static files for the server (css, scripts, img) */
+// every '.use()' function its a server config settings //
+// config the static files for the server (css, scripts, img) //
 server.use(express.static("public"))
-/* ROUTES CONFIG */
-/* INDEX Page */
+
+// ROUTES CONFIG (endpoints) //
+// * INDEX Page //
 .get("/", pageLanding)
-/* STUDY Page */
+// * STUDY Page //
 .get("/study", pageStudy)
-/* GIVE CLASSES Page */
+// * GIVE CLASSES Page //
 .get("/give-classes", pageGiveClasses)
-/* PORT CONFIG */
+// PORT CONFIG //
 .listen(5500);
