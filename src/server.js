2bc2f3345b9b8994ab34a40b144c8956802caecf
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-08-06 10:51:11 -0300

    basic routes config index study give-classes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
-require('express')()
+/* const var to shortly call express when it's needed */
+const express = require('express');
+/* const var to server expressions use */
+const server = express();
+/* every '.use()' function its a server config settings */
+
+/* config the static files for the server */
+server.use(express.static("public"))
+
 /* setting the route 'get', to return the second parameter */
 /* 'req' is basically the info you are sending to the endpoint */ 
 /* 'res' is the return of this endpoint */
+/* '__dirname' is a node var which return the root path */
+
+
+/* ROUTES CONFIG */
+/* INDEX Page */
 .get("/", (req, res) => {
-    return res.send("Hi from NLW")
+    return res.sendFile(__dirname + "/views/index.html")
 })
+/* STUDY Page */
 .get("/study", (req, res) => {
-    return res.send("Página espelho")
+    return res.sendFile(__dirname + "/views/study.html")
 })
-.listen(5500)
+/* GIVE CLASSES Page */
+.get("/give-classes", (req, res) => {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+})
+/* PORT CONFIG */
+.listen(5500);
