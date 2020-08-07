c054cd1b844a67f8e9fce9f90ff11c84248c6552
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-08-07 18:09:56 -0300

    fix on insert into and select from classes all project now is fully workinggit add .!

--- src/pages.js
+++ src/pages.js
@@ -46,6 +46,10 @@ async function pageStudy(req, res) {
         const db = await Database;
         const proffys = await db.all(query);
 
+        proffys.map((proffy) => {
+            proffy.subject = getSubject(proffy.subject)
+        });
+        
         return res.render('study.html', { proffys, subjects, filters, weekdays })
 
     } catch (error) {
@@ -53,32 +57,52 @@ async function pageStudy(req, res) {
     }
 }
 
-
 // Give-Classes Page Config //
 function pageGiveClasses(req, res) {
-
-    // Storing the data that we are sending through the 'req' parameter //
-    // basically this 'req' will have all form data from the Give-Classes //
-    // when the 'save' button is clicked. //
-    const data = req.query;
-    // Checking if the data returned is not null (empty) //
-    const isNotEmpty = Object.keys(data).length > 0;
-
-    // If the data is complete, we are going to record this new Proffy user //
-    if(isNotEmpty) {
-        // Consulting the name of the subject, using the function bellow. //
-        data.subject = getSubject(data.subject);
-        // Pushing the new data into the Proffys array. //
-        proffys.push(data);
-        // Redirect to the Study page. //
-        return res.redirect("/study");
-    }
-    
     return res.render("give-classes.html", { subjects, weekdays })
+}
+
+async function saveClasses(req, res) {
+    const createProffy = require('./database/createProffy');
+    
+    const proffyValue = {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio: req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekday.map((weekday, index) => {
+        return {
+            weekday,
+            time_from: convertHoursToMinutes(req.body.time_from[index]),
+            time_to: convertHoursToMinutes(req.body.time_to[index])
+        }
+    })
+
+    try {
+        const db = await Database;
+        await createProffy(db, { proffyValue, classValue, classScheduleValues });
+
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+
+        return res.redirect("/study" + queryString);
+
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
     pageLanding,
     pageStudy,
-    pageGiveClasses
+    pageGiveClasses,
+    saveClasses
 }
--- src/server.js
+++ src/server.js
@@ -13,13 +13,17 @@ nunjucks.configure('src/views', {
     noCache: true
 })
 // Importing pages //
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const { pageLanding,
+        pageStudy,
+        pageGiveClasses,
+        saveClasses } = require('./pages')
 
 
 // every '.use()' function its a server config settings //
 // config the static files for the server (css, scripts, img) //
 server.use(express.static("public"))
-
+// Receive data from body req //
+.use(express.urlencoded({ extended: true }))
 // ROUTES CONFIG (endpoints) //
 // * INDEX Page //
 .get("/", pageLanding)
@@ -27,5 +31,7 @@ server.use(express.static("public"))
 .get("/study", pageStudy)
 // * GIVE CLASSES Page //
 .get("/give-classes", pageGiveClasses)
+// * STUDY Page - POST ROUTE //
+.post("/save-classes", saveClasses)
 // PORT CONFIG //
 .listen(5500);
